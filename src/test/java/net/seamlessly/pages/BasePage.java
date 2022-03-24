64883134fe84bdc1238392cec93275b8e53b8da8
Aysun120 <aysundamar114@gmail.com>
2022-03-24 14:21:19 -0500

    initial commit

---
+++ src/test/java/net/seamlessly/pages/BasePage.java
@@ -0,0 +1,11 @@
+package net.seamlessly.pages;
+
+import net.seamlessly.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+}
